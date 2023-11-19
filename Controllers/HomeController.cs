e6af155cb48878deaaf6faae93f9657fdad3d5a1
Bryndikov <151222091+Bryndikov@users.noreply.github.com>
2023-11-19 21:24:40 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using WebAppLab.Services.EmailServices;
+using WebLabApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace WebLabApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+		private readonly ISender _emailSender;
+		public HomeController(ILogger<HomeController> logger, ISender emailSender)
+		{
+			_logger = logger;
+			_emailSender=emailSender;
+		}
+        public IActionResult Index()
+        {
+            _logger.LogInformation("This is an information log from HomeController.");
+            return View();
+        }
+        [HttpPost]
+		public async Task<IActionResult> SendEmail([FromBody] EmailModel vm)
+		{
+			var _vm = vm;
+			if (_vm == null)
+			{
+				return BadRequest();
+			}
+			await _emailSender.SendEmailAsync(_vm.Email, "Test", _vm.MessageText);
+			return Ok();
+		}
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using WebAppLab.Services.EmailServices;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<ISender, Sender>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
